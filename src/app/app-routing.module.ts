8cc2eac2e72bddd20f9ef71a52a86abf9932fa3d
Salomon736 <salomon.mena@uab.edu.bo>
2025-08-18 13:03:56 -0400

    feat(menu):Add component for menu with CRUD

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { DishMealPeriodManagementComponent } from './features/restaurant-gustov/views/dish-and-meal-period/index/dish-mealperiod-management.component';
 import { HomeComponent } from './home/home.component';
+import { MenuManagementComponent } from './features/restaurant-gustov/views/menu/index/menu-index.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'dish-meal-period-management', component: DishMealPeriodManagementComponent },
-  // { path: 'menu-management', component: MenuManagementComponent },
+  { path: 'menu-management', component: MenuManagementComponent },
   { path: '**', redirectTo: '' }
 ];
 
